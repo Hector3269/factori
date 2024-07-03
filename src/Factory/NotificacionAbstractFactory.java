3e353dd0cb556f7db60b228325bcbceff47ead51
Hector3269 <danielramirezr27@gmail.comm>
2024-07-02 20:54:56 -0600

    first commit

---
+++ src/Factory/NotificacionAbstractFactory.java
@@ -0,0 +1,7 @@
+package Factory;
+// Defino la Interfaz de Abstract Factory
+public interface NotificacionAbstractFactory {
+    Notificacion crearEmailNotificacion();
+    Notificacion crearSMSNotificacion();
+    Notificacion crearAppNotificacion();
+}
